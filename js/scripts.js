e547dc51e2198f6ef376fa119cdc0af098bdd60b
Andrea Berrocal <andrea.berrocal95@gmail.com>
2021-10-28 21:36:16 -0700

    js scripts

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-const cursor = document.querySelector(".cursor");
-const cursor2 = document.querySelector(".cursor2");
-
-window.addEventListener("mousemove", function(e){
-    cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
-
-});
