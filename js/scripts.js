0826d3e07d6de0052d4b818ed2dcf16d9f8958cc
Andrea Berrocal <andrea.berrocal95@gmail.com>
2021-10-28 21:31:36 -0700

    cards - removed  BG color, added shadow

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,6 @@ const cursor = document.querySelector(".cursor");
 const cursor2 = document.querySelector(".cursor2");
 
 window.addEventListener("mousemove", function(e){
-    cursor.style.cssText = cursor2.style.cssText = "left: " + e.pageX + "px; top: " + e.pageY + "px;";
+    cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
 
 });
